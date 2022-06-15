9cf79f37bdfa5a04c2c611aca9ed40bed7fc8bf5
Zhanglab-IOZ <74943272+Zhanglab-IOZ@users.noreply.github.com>
2022-06-15 11:18:37 +0800

    Add files via upload

--- Fig4/PFI_pancancer_analysis.R
+++ Fig4/PFI_pancancer_analysis.R
@@ -1,13 +1,18 @@
 library(dplyr)
 library(ggplot2)
 library(egg)
+library(reshape2)
 
+setwd("/rd/suth/2022/02March_Cancer_evo/Fig4a")
 load("integrate_DE_info_result.rds")
 load("integrate_DE_result.rds")
-load("integrate_cox_result.rds")
 
-integrate_cox_result<-integrate_cox_result%>%select(gene,multi_p_value,multi_HR,multi_fdr,cancer_type)
-integrate_cox_result<-integrate_cox_result%>%mutate(cox_class=ifelse(multi_HR>0&multi_fdr<=0.05,"up",ifelse(multi_HR<0&multi_fdr<=0.05,"down","no")))
+load("integrate_cox_result.rds")
+names(integrate_cox_result)<-gsub("exp","uni",names(integrate_cox_result))
+
+integrate_cox_result<-integrate_cox_result%>%select(gene,uni_p_value,uni_HR,uni_fdr,cancer_type)
+
+integrate_cox_result<-integrate_cox_result%>%mutate(cox_class=ifelse(uni_HR>0&uni_fdr<=0.05,"up",ifelse(uni_HR<0&uni_fdr<=0.05,"down","no")))
 
 load("tau_SPM_age_chr_info_hybrid_result.rds")
 
@@ -22,28 +27,44 @@ integrate_cox_result_number<-merge(integrate_cox_result_number,tau_SPM_age_chr_i
 PSG_up<-integrate_cox_result_number%>%filter(phylo_age=="Primate",DE_class=="up")%>%.$gene
 EM_down<-integrate_cox_result_number%>%filter(phylo_age_class=="EM",DE_class=="down")%>%.$gene
 
-
 UC_up<-integrate_cox_result_number%>%filter(as.numeric(phylo_age)<=3,DE_class=="up")%>%.$gene
 
 pie_data<-rbind(data.frame(signal_num=c(nrow(integrate_cox_result_number%>%filter(gene%in%UC_up,cox_net>0)),nrow(integrate_cox_result_number%>%filter(gene%in%UC_up,cox_net<0)),nrow(integrate_cox_result_number%>%filter(gene%in%UC_up,cox_num>0,cox_net==0)),nrow(integrate_cox_result_number%>%filter(gene%in%UC_up,cox_num==0))),all_num=nrow(integrate_cox_result_number%>%filter(gene%in%UC_up)),class="UC"),data.frame(signal_num=c(nrow(integrate_cox_result_number%>%filter(gene%in%EM_down,cox_net>0)),nrow(integrate_cox_result_number%>%filter(gene%in%EM_down,cox_net<0)),nrow(integrate_cox_result_number%>%filter(gene%in%EM_down,cox_num>0,cox_net==0)),nrow(integrate_cox_result_number%>%filter(gene%in%EM_down,cox_num==0))),all_num=nrow(integrate_cox_result_number%>%filter(gene%in%EM_down)),class="EM"),data.frame(signal_num=c(nrow(integrate_cox_result_number%>%filter(cox_net>0)),nrow(integrate_cox_result_number%>%filter(cox_net<0)),nrow(integrate_cox_result_number%>%filter(cox_num>0,cox_net==0)),nrow(integrate_cox_result_number%>%filter(cox_num==0))),all_num=nrow(integrate_cox_result_number),class="BG"),data.frame(signal_num=c(nrow(integrate_cox_result_number%>%filter(gene%in%PSG_up,cox_net>0)),nrow(integrate_cox_result_number%>%filter(gene%in%PSG_up,cox_net<0)),nrow(integrate_cox_result_number%>%filter(gene%in%PSG_up,cox_num>0,cox_net==0)),nrow(integrate_cox_result_number%>%filter(gene%in%PSG_up,cox_num==0))),all_num=nrow(integrate_cox_result_number%>%filter(gene%in%PSG_up)),class="PSG"))
+
+
 pie_data<-data.frame(pie_data%>%mutate(prop=signal_num/all_num))
 pie_data$group<-rep(c("up","down","intermediate","no"),4)
 pie_data$group<-factor(pie_data$group,levels=c("up","no","intermediate","down"))
 
-pie_data
-pie_EM<-ggplot()+geom_bar(data=pie_data%>%filter(class=="EM"),aes(x="",y=prop,fill=group),stat="identity", size=2,width=1, color="white",alpha=.5)+coord_polar("y",start=0)+theme_void()+scale_fill_manual(values=c("violetred1","grey30","grey75","palegreen3"))+theme(legend.text=element_text(size=15),legend.title=element_blank(),legend.position="none")
-pie_PSG<-ggplot()+geom_bar(data=pie_data%>%filter(class=="PSG"),aes(x="",y=prop,fill=group),stat="identity", size=2,width=1, color="white",alpha=.5)+coord_polar("y",start=0)+theme_void()+scale_fill_manual(values=c("violetred1","grey30","grey75","palegreen3"))+theme(legend.text=element_text(size=15),legend.title=element_blank(),legend.position="none")
-pie_BG<-ggplot()+geom_bar(data=pie_data%>%filter(class=="BG"),aes(x="",y=prop,fill=group),stat="identity", size=2,width=1, color="white",alpha=.5)+coord_polar("y",start=0)+theme_void()+scale_fill_manual(values=c("violetred1","grey30","grey75","palegreen3"))+theme(legend.text=element_text(size=15),legend.title=element_blank(),legend.position="none")
-pie_UC<-ggplot()+geom_bar(data=pie_data%>%filter(class=="UC"),aes(x="",y=prop,fill=group),stat="identity", size=2,width=1, color="white",alpha=.5)+coord_polar("y",start=0)+theme_void()+scale_fill_manual(values=c("violetred1","grey30","grey75","palegreen3"))+theme(legend.text=element_text(size=15),legend.title=element_blank(),legend.position="none")
+pie_data <- pie_data |> filter(group != "intermediate")
+
+pie_EM<-ggplot()+geom_bar(data=pie_data%>%filter(class=="EM"),aes(x="",y=prop,fill=group),stat="identity", size=0.1,width=1, color="white",alpha=.5)+coord_polar("y",start=0)+theme_void()+scale_fill_manual(values=c("violetred1","grey30",
+#    "grey75",
+    "palegreen3"))+theme(legend.text=element_text(size=15),legend.title=element_blank(),legend.position="none")
+
+pie_PSG<-ggplot()+geom_bar(data=pie_data%>%filter(class=="PSG"),aes(x="",y=prop,fill=group),stat="identity", size=0.1,width=1, color="white",alpha=.5)+coord_polar("y",start=0)+theme_void()+scale_fill_manual(values=c("violetred1","grey30",
+#    "grey75",
+    "palegreen3"))+theme(legend.text=element_text(size=15),legend.title=element_blank(),legend.position="none")
+
+pie_BG<-ggplot()+geom_bar(data=pie_data%>%filter(class=="BG"),aes(x="",y=prop,fill=group),stat="identity", size=0.1,width=1, color="white",alpha=.5)+coord_polar("y",start=0)+theme_void()+scale_fill_manual(values=c("violetred1","grey30",
+#    "grey75",
+    "palegreen3"))+theme(legend.text=element_text(size=15),legend.title=element_blank(),legend.position="none")
+
+pie_UC<-ggplot()+geom_bar(data=pie_data%>%filter(class=="UC"),aes(x="",y=prop,fill=group),stat="identity", size=0.1,width=1, color="white",alpha=.5)+coord_polar("y",start=0)+theme_void()+scale_fill_manual(values=c("violetred1","grey30",
+#    "grey75",
+    "palegreen3"))+theme(legend.text=element_text(size=15),legend.title=element_blank(),legend.position="none")
+
 pie_blank<-ggplot()
-#ggplot2::ggsave(file="PFI_fdr_0.05_UC.pdf",ggarrange(pie_UC,pie_BG,pie_PSG,pie_EM,nrow=2,ncol=2,widths=c(1,1),heights=c(1,1),draw=F),width=5.5,height=5.5,device="pdf")
 
-ggplot2::ggsave(file="PFI_pancancer.pdf",ggarrange(pie_blank,pie_EM,pie_UC,pie_PSG,nrow=2,ncol=2,widths=c(1,1),heights=c(1,1),draw=F),width=4,height=4,device="pdf")
-
+ggplot2::ggsave(file="fig4a.pdf",ggarrange(pie_blank,pie_EM,pie_UC,pie_PSG,nrow=2,ncol=2,widths=c(1,1),heights=c(1,1),draw=F),width=4,height=4,device="pdf")
 
 chi_pvalue_PSG_BG<-as.numeric(chisq.test(pie_data%>%filter(class=="PSG")%>%.$signal_num,p=c(pie_data%>%filter(class=="BG")%>%.$signal_num),rescale.p=T)%>%.$p.value)
+
 chi_pvalue_PSG_EM<-as.numeric(chisq.test(pie_data%>%filter(class=="PSG")%>%.$signal_num,p=c(pie_data%>%filter(class=="EM")%>%.$signal_num),rescale.p=T)%>%.$p.value)
+
 chi_pvalue_EM_BG<-as.numeric(chisq.test(pie_data%>%filter(class=="EM")%>%.$signal_num,p=c(pie_data%>%filter(class=="BG")%>%.$signal_num),rescale.p=T)%>%.$p.value)
+
 chi_pvalue_UC_EM<-as.numeric(chisq.test(pie_data%>%filter(class=="UC")%>%.$signal_num,p=c(pie_data%>%filter(class=="EM")%>%.$signal_num),rescale.p=T)%>%.$p.value)
+
 chi_pvalue_UC_PSG<-as.numeric(chisq.test(pie_data%>%filter(class=="PSG")%>%.$signal_num,p=c(pie_data%>%filter(class=="UC")%>%.$signal_num),rescale.p=T)%>%.$p.value)
 
